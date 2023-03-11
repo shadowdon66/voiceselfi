c25e428a106d478107008b435291658a0b7c6706
bob <92489084+shadowdon66@users.noreply.github.com>
2023-03-11 09:51:27 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -41,19 +41,19 @@ function speak(){
     }, 5000);
     
 }
-
+camera = document.getElementById("camera");
 Webcam.set({
     width:360,
     height:250,
     image_format : 'png',
     png_quality:90
 });
-camera = document.getElementById("camera");
+
 
 function take_snapshot()
 {
     Webcam.snap(function(data_uri) {
-        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
     });
 }
 
